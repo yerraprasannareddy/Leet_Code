87392b07b3853ac1527cfb28422041392490bca3
yerraprasannareddy <160003975+yerraprasannareddy@users.noreply.github.com>
2025-06-03 08:49:24 +0530

    Time: 5 ms (69.85%), Space: 44.1 MB (62.62%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int n=x;
+        int revNum=0;
+        while(n!=0){
+            int d = n%10;
+            revNum= revNum*10+d;
+            n = n/10;
+        }
+        if(x==revNum){
+            return true;
+        }
+        return false;
+        
+    }
+}
