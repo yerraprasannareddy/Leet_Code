90aca44b11677902eaba082c5654eadc34e8f638
yerraprasannareddy <160003975+yerraprasannareddy@users.noreply.github.com>
2025-06-09 08:59:24 +0530

    Time: 0 ms (100%), Space: 40.2 MB (94.36%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,17 +1,7 @@
 // Josephus problem
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=1;i<=n;i++){
-            queue.add(i);
-        }
-        while(queue.size()>1){
-            for(int i=1;i<k;i++){
-                queue.add(queue.remove());
-            }
-            queue.remove();
-
-        }
-        return queue.peek();
+         if(n==1) return 1;
+         return(findTheWinner(n-1,k)+k-1)%n+1;
     }
 }
